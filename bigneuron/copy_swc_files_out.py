b891bff9fc915287b3a0e87f5978031c6414a7ab
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2016-07-01 15:32:12 -0700

    More scripts for image profiling.

---
+++ bigneuron/copy_swc_files_out.py
@@ -0,0 +1,20 @@
+__author__ = 'xiaoxiaol'
+import os
+### main
+data_DIR = "/data/mat/xiaoxiaol/data/big_neuron/silver/0401_gold163_all_soma_sort"
+output_dir = data_DIR
+#run_consensus(data_DIR, output_dir)
+
+
+destnation_DIR = "/data/mat/xiaoxiaol/data/big_neuron/silver/0401_gold163_all_soma_sort_strict_swc_only"
+
+
+subdirs = [x[0] for x in os.walk(data_DIR)]
+for recon_dir in subdirs[1:]:
+        folder_name = recon_dir.split('/')[-1]
+        if 'processed' in  folder_name:
+                  subfolderpath=recon_dir.split('/')[-2]
+                  print subfolderpath
+                  os.system('mkdir -p '+destnation_DIR+'/'+subfolderpath+'/processed' )
+                  os.system('cp '+ recon_dir+'/*.strict.swc  ' + destnation_DIR+'/'+subfolderpath+'/processed/')
+                  os.system('cp '+ recon_dir+'/../*.strict.swc  ' + destnation_DIR+'/'+subfolderpath+'/')
--- bigneuron/image_profiling_analysis.py
+++ bigneuron/image_profiling_analysis.py
@@ -8,11 +8,12 @@ import pandas as pd
 
 data_DIR = "/data/mat/xiaoxiaol/data/big_neuron/silver/0401_gold163_all_soma_sort"
 #df_metrics = pd.read_csv(data_DIR+'/image_profiling_p0.05_original_gs.csv')
-df_metrics = pd.read_csv(data_DIR+'/radius_estimation_profiling.csv')
+df_metrics = pd.read_csv(data_DIR+'/radius_estimation_profiling_final.csv')
 
 #
 df_meta = pd.read_csv(data_DIR+'/image_name_lookup_table_with_limited_meta.csv')
 df_data= pd.merge(df_metrics,df_meta, on='image_id' )
+df_data['image_id']=df_data['image_id'].astype('int')
 df_data["meta"] =df_data["species"]+ "-"+df_data["lab"]+ " ("+df_data["image_id"].map(str)+")"
 #df_data.to_csv(data_DIR+'/image_profiling_p0.05_original_gs_with_meta.csv', index=False)
 df_data.to_csv(data_DIR+'/image_profiling_p0.05_reestimated_radius_gs_with_meta.csv', index=False)
@@ -25,16 +26,20 @@ df_data.loc[df_data['CNR'] >100, 'CNR'] = 100
 sb.set_context("poster")
 
 
-f, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(30, 15), sharex=True)
+f, (ax1, ax2, ax3,ax4) = plt.subplots(4, 1, figsize=(30, 15), sharex=True)
+
 sb.barplot(data=df_data,x='image_id', y='CNR', order = df_data.image_id,    ax=ax1)
 #   plt.xticks(range(len(df_data)), df_data['image_id'], rotation='vertical')
 
-sb.barplot(data=df_data,x='image_id', y='Tubularity', order = df_data.image_id,    ax=ax2)
+sb.barplot(data=df_data,x='image_id', y='SNR', order = df_data.image_id,    ax=ax2)
+#   plt.xticks(range(len(df_data)), df_data['image_id'], rotation='vertical')
+
+sb.barplot(data=df_data,x='image_id', y='mean_tubularity', order = df_data.image_id,    ax=ax3)
 #plt.xticks(range(len(df_data)), df_data['meta'], rotation='vertical')
 
 ax1.set_xlabel('')
 ax2.set_xlabel('')
-sb.barplot(data=df_data,x='image_id', y='Dynamic_Range', order = df_data.image_id,    ax=ax3)
+sb.barplot(data=df_data,x='image_id', y='dynamic_range', order = df_data.image_id,    ax=ax4)
 plt.xticks(range(len(df_data)), df_data['meta'], rotation='vertical')
 
 sb.despine(bottom=True)
